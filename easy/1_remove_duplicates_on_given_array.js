e83173892522c35099b3d717e246ac507e7775f4
viktor.may <may.viktor@gmail.com>
2018-11-19 22:01:50 +0300

    own 2nd variant

--- easy/1_remove_duplicates_on_given_array.js
+++ easy/1_remove_duplicates_on_given_array.js
@@ -1,8 +1,9 @@
-let nums = [0,0,1,1,1,2,2,3,3,4];
+
+let nums = [1, 2, 3, 1, 4, , 2, 3, 4, 4, 4, 4, 4];
 
 const removeDuplicates = (nums) => {
   let resultLength = null;
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i += 1) {
       if (nums[i] === nums[i + 1]) {
         nums.splice(i, 1);
         i -= 1;        
@@ -12,27 +13,26 @@ const removeDuplicates = (nums) => {
   return resultLength + 1;
 };
 
-console.log('length -', removeDuplicates(nums));
+const removeDuplicatesWhile = (nums) => {
+  const storage = {}
+  let index = nums.length - 1
 
-console.log('nums -', nums);
-
-// ===================
-
-let arr = [0,0,1,1,1,2,2,3,3,4];
-
-const removeDuplicates2 = (arr) => {
-  const obj = {};
-  let len = arr.length;
-  let i = 0;
-
-  while (i <= len) {
-    if (!obj[arr[i]]) {
-      obj[arr[i]] = true;
-      i += 1;
+  while (index >= 0) {
+    if (nums[index] && !storage[nums[index]]) {
+      storage[nums[index]] = true
     } else {
-      arr.splice(i, 1);
-      len -= 1;
+      nums.splice(index, 1);
     }
+    index -= 1
   }
+  return nums
 }
+
+console.time('timer');
+console.log('while loop -', removeDuplicatesWhile(nums));
+console.timeEnd('timer');
+
+console.time('timer');
+console.log('for loop -', removeDuplicates(nums));
+console.timeEnd('timer');
 
